58aa16b779aa90e2d7020ce5f38d0b4e428a4ecb
Benja <be.aedot@duocuc.cl>
2026-05-11 15:54:19 -0400

    feat: implementa ms-auth con JWT, Spring Security y usuarios

---
+++ src/main/java/cl/donaton/msauth/service/UsuarioDetailsService.java
@@ -0,0 +1,21 @@
+package cl.donaton.msauth.service;
+
+import cl.donaton.msauth.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + email));
+    }
+}
